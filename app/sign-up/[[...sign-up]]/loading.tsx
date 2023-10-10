aeaaf3e24481b7cc747bfadfb4bd5a4f4289064f
Alan Muñoz <alanjmr206@gmail.com>
2023-10-10 16:46:05 -0700

    add loading file for sign up

---
+++ app/sign-up/[[...sign-up]]/loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div>
+        <img className="animate-spin" src="/loading.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
