83dfb595df686d146ce9e9c7da2b2a8b0077a720
Alan Muñoz <alanjmr206@gmail.com>
2023-10-11 16:10:27 -0700

    change margin top for sign up component

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function SignUpForm() {
   return (
     <div className="flex h-[55rem] justify-center items-center bg-zinc-50">
-      <div className="flex flex-col justify-center mt-10 items-center">
+      <div className="flex flex-col justify-center mt-20 items-center">
         <SignUp
           appearance={{
             elements: {
