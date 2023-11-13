371acfdd017f684b461e586b60085f9f3872da79
Alan Muñoz <alanjmr206@gmail.com>
2023-11-13 13:20:28 -0700

    modify filter side bar height

--- app/components/Filters/FilterSideBar.tsx
+++ app/components/Filters/FilterSideBar.tsx
@@ -2,7 +2,7 @@ import FilterContent from "./FilterContent";
 
 const FilterSideBar = () => {
   return (
-    <div className="sticky z-0 top-40 mr-5 mt-8 pl-10 hidden lg:block lg:min-w-[25rem] h-[45rem] overflow-y-auto overflow-x-hidden scrollbar">
+    <div className="sticky top-0 mr-5 mt-8 pl-10 hidden lg:block lg:min-w-[25rem] h-screen overflow-y-auto scrollbar">
       <div className="mb-5">
         <p className="text-2xl">Filters</p>
       </div>
