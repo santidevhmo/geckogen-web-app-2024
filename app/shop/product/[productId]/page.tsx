1bc0f99f2560e311660651e86bbad48b1754c9a9
Alan Muñoz <alanjmr206@gmail.com>
2023-11-23 00:41:35 -0700

    add getProductData func to get product info
    - remove useEffect to fecth data
    - use fetch inside an async function outside of Product component

--- app/shop/product/[productId]/page.tsx
+++ app/shop/product/[productId]/page.tsx
@@ -1,91 +1,69 @@
-"use client";
-
-import ProductDetailSkeleton from "@/app/components/Skeleton/ProductDetailSkeleton";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import Image from "next/image";
 
-interface ProductData {
-  productName: string;
-  productPrice: number;
-  productPriceId: string;
-  productImage: string[];
-  productDescription: string;
-}
+const getProductData = async (productId: string) => {
+  const response = await fetch(
+    `${process.env.DOMAIN}/api/products?id=${productId}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-const Product = ({ params }: { params: { productId: string } }) => {
-  const [product, setProduct] = useState<ProductData>();
+  return response.json();
+};
 
-  useEffect(() => {
-    const getProduct = async () => {
-      try {
-        const response = await fetch(`/api/products?id=${params.productId}`);
-        if (response.ok) {
-          const data = await response.json();
-          setProduct(data);
-        } else {
-          console.error("Failed to fetch product");
-        }
-      } catch (error) {
-        console.error("Error fetching product:", error);
-      }
-    };
-
-    getProduct();
-  }, []);
+const Product = async ({ params }: { params: { productId: string } }) => {
+  const product = await getProductData(params.productId);
 
   return (
     <div className="pt-24 pb-14 flex justify-center lg:items-center lg:h-[50rem]">
-      {product ? (
-        <div className="w-[30rem] lg:flex lg:w-auto lg:gap-10 px-2">
-          <div className="space-y-1 pb-7 lg:hidden">
+      <div className="w-[30rem] lg:flex lg:w-auto lg:gap-10 px-2">
+        <div className="space-y-1 pb-7 lg:hidden">
+          <p className="text-2xl md:text-3xl">{product.productName}</p>
+          <p className="text-xl md:text-2xl">${product.productPrice / 100}</p>
+        </div>
+
+        <div>
+          <Image
+            className="object-cover rounded-lg"
+            width={432}
+            height={432}
+            src={product.productImage[0]}
+            alt=""
+          />
+        </div>
+
+        <div className="pt-4 text-base text-gray-600 w-fit lg:hidden">
+          <p>{product.productDescription ?? "No description available"}</p>
+        </div>
+
+        <div className="pt-12 lg:hidden">
+          <Link href={`/checkout/${product.productPriceId}`}>
+            <button className="py-3 w-full bg-blue-500 text-white rounded-md">
+              Buy
+            </button>
+          </Link>
+        </div>
+
+        <div className="hidden lg:block">
+          <div className="space-y-1 pb-7">
             <p className="text-2xl md:text-3xl">{product.productName}</p>
             <p className="text-xl md:text-2xl">${product.productPrice / 100}</p>
           </div>
 
-          <div>
-            <img
-              className="w-full object-cover rounded-lg"
-              src={product.productImage[0]}
-              alt=""
-            />
-          </div>
-
-          <div className="pt-4 text-base text-gray-600 w-fit lg:hidden">
+          <div className="pt-4 text-base text-gray-600 w-96">
             <p>{product.productDescription ?? "No description available"}</p>
           </div>
 
-          <div className="pt-12 lg:hidden">
+          <div className="pt-12">
             <Link href={`/checkout/${product.productPriceId}`}>
               <button className="py-3 w-full bg-blue-500 text-white rounded-md">
                 Buy
               </button>
             </Link>
           </div>
-
-          <div className="hidden lg:block">
-            <div className="space-y-1 pb-7">
-              <p className="text-2xl md:text-3xl">{product.productName}</p>
-              <p className="text-xl md:text-2xl">
-                ${product.productPrice / 100}
-              </p>
-            </div>
-
-            <div className="pt-4 text-base text-gray-600 w-96">
-              <p>{product.productDescription ?? "No description available"}</p>
-            </div>
-
-            <div className="pt-12">
-              <Link href={`/checkout/${product.productPriceId}`}>
-                <button className="py-3 w-full bg-blue-500 text-white rounded-md">
-                  Buy
-                </button>
-              </Link>
-            </div>
-          </div>
         </div>
-      ) : (
-        <ProductDetailSkeleton />
-      )}
+      </div>
     </div>
   );
 };
