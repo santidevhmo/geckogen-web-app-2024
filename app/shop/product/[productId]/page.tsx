f91b9585f05722159b5bcfdbb32a7fffaab75b64
Alan Muñoz <alanjmr206@gmail.com>
2023-11-23 16:23:58 -0700

    add back to shop button

--- app/shop/product/[productId]/page.tsx
+++ app/shop/product/[productId]/page.tsx
@@ -17,50 +17,69 @@ const Product = async ({ params }: { params: { productId: string } }) => {
 
   return (
     <div className="pt-24 pb-14 flex justify-center lg:items-center lg:h-[50rem]">
-      <div className="w-[30rem] lg:flex lg:w-auto lg:gap-10 px-2">
-        <div className="space-y-1 pb-7 lg:hidden">
-          <p className="text-2xl md:text-3xl">{product.productName}</p>
-          <p className="text-xl md:text-2xl">${product.productPrice / 100}</p>
-        </div>
-
-        <div>
-          <Image
-            className="object-cover rounded-lg"
-            width={432}
-            height={432}
-            src={product.productImage[0]}
-            alt=""
-          />
-        </div>
-
-        <div className="pt-4 text-base text-gray-600 w-fit lg:hidden">
-          <p>{product.productDescription ?? "No description available"}</p>
-        </div>
-
-        <div className="pt-12 lg:hidden">
-          <Link href={`/checkout/${product.productPriceId}`}>
-            <button className="py-3 w-full bg-blue-500 text-white rounded-md">
-              Buy
-            </button>
+      <div className="px-2 w-[26rem] lg:w-auto">
+        <div className="mb-8 text-blue-500 w-32 py-1 hover:bg-gray-100 rounded-md">
+          <Link href={"/shop"}>
+            <div className="flex items-center gap-2">
+              <div>
+                <img
+                  className="h-4 w-4"
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAArElEQVR4nO3ZsQ3CQBBE0WkCZOo0a2npjgiBIMEBIUi3gjJACxE0wOxpXgUefQe+MyAi/MbHElM7wWKL2iNixhRPWOxRkt8XsDh/RrQL1tcB5WgEC5VgoRIsVIKFSrBQCRauD0ASrhIkvIcSydrxPSKPqXlcLct6GeI/r9bmtkJZrjGkXGVIucqQcpUhpTKsVIaVyrBSGVZdlRm/fk8fUFqOyTsAi92/H0WkVy/ksYTxlICpRQAAAABJRU5ErkJggg=="
+                />
+              </div>
+              <div>
+                <p>Back to shop</p>
+              </div>
+            </div>
           </Link>
         </div>
-
-        <div className="hidden lg:block">
-          <div className="space-y-1 pb-7">
+        <div className="lg:flex lg:gap-10">
+          <div className="space-y-1 pb-7 lg:hidden">
             <p className="text-2xl md:text-3xl">{product.productName}</p>
             <p className="text-xl md:text-2xl">${product.productPrice / 100}</p>
           </div>
 
-          <div className="pt-4 text-base text-gray-600 w-96">
+          <div>
+            <Image
+              className="object-cover rounded-lg"
+              width={432}
+              height={432}
+              src={product.productImage[0]}
+              alt=""
+            />
+          </div>
+
+          <div className="pt-4 text-base text-gray-600 w-fit lg:hidden">
             <p>{product.productDescription ?? "No description available"}</p>
           </div>
 
-          <div className="pt-12">
+          <div className="pt-12 lg:hidden">
             <Link href={`/checkout/${product.productPriceId}`}>
               <button className="py-3 w-full bg-blue-500 text-white rounded-md">
                 Buy
               </button>
             </Link>
+          </div>
+
+          <div className="hidden lg:block">
+            <div className="space-y-1 pb-7">
+              <p className="text-2xl md:text-3xl">{product.productName}</p>
+              <p className="text-xl md:text-2xl">
+                ${product.productPrice / 100}
+              </p>
+            </div>
+
+            <div className="pt-4 text-base text-gray-600 w-96">
+              <p>{product.productDescription ?? "No description available"}</p>
+            </div>
+
+            <div className="pt-12">
+              <Link href={`/checkout/${product.productPriceId}`}>
+                <button className="py-3 w-full bg-blue-500 text-white rounded-md">
+                  Buy
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
