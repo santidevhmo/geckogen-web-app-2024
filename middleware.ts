029a59d619816a5cbf7c791ab079040dc964c3b4
Alan Muñoz <alanjmr206@gmail.com>
2023-11-22 17:48:02 -0700

    remove public routes from clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-      publicRoutes: ["/","/shop", "/api/prices", 
-                     "/api/checkout", "/checkout", 
-                     "/return", "/shop/product/:id", 
-                     "/api/products", "/shop/product/api/products",
-                     "/checkout/:id", "/orders"]
+  publicRoutes: [
+    "/",
+    "/shop",
+    "/shop/product/:id",
+    "/api/products",
+  ]
 });
- 
+
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
