88ce3c9ea78fe5e9dcb2c3e4fde0b169b36636b0
Alan Muñoz <alanjmr206@gmail.com>
2023-11-24 18:50:40 -0700

    change filter side bar size

--- app/components/Filters/FilterSideBar.tsx
+++ app/components/Filters/FilterSideBar.tsx
@@ -2,7 +2,7 @@ import FilterContent from "./FilterContent";
 
 const FilterSideBar = ({filterCount}: {filterCount: number}) => {
   return (
-    <div className="mr-5 mt-8 pl-10 hidden lg:block lg:min-w-[21rem] h-screen overflow-y-auto scrollbar">
+    <div className="mr-5 mt-8 pl-10 hidden lg:block lg:min-w-[23rem] h-screen overflow-y-auto scrollbar">
       <div className="mb-5">
         {filterCount > 0 ?
         <p className="text-2xl">{`Filters (${filterCount})`}</p>
