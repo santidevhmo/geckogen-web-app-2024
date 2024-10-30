494e2d6e0bc6c9effea73189d12a40d6f2933796
santidevhmo <santiagoagdc42@gmail.com>
2024-10-29 18:31:15 -0700

    delete substackWidget due to error deploymeny purposes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import SubstackBlogRow from './substackWidget';
+// import SubstackBlogRow from './substackWidget';
 
 export default function Blog() {
 
@@ -25,7 +25,8 @@ export default function Blog() {
                 <p className="font-light text-gray-600 text-center w-3/4 lg:w-2/4 mb-10">
                     At Geckogen, we explore the fascinating realm of geckos, from vibrant morphs to cutting-edge genetic research. Our mission is to foster a deeper connection between humans and reptiles by promoting responsible guardianship and species preservation. Dive into our blog for expert insights on gecko care, breeding, and the science driving the future of reptile conservation.
                 </p>
-                <SubstackBlogRow substackUrl='https://geckogen.substack.com' />
+                <h2>Substack blog row component soon</h2>
+                {/* <SubstackBlogRow substackUrl='https://geckogen.substack.com' /> */}
             </div>
 
         </div>
