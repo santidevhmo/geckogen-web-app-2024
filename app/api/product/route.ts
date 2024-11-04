1e76894749fcc4c357cbfe6f5940f2b6e87d6c88
santidevhmo <santiagoagdc42@gmail.com>
2024-11-03 18:47:01 -0700

    re-design individual product & product card equal sizes

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -3,23 +3,27 @@ import { Stripe } from "stripe";
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_API_SECRET_KEY as string, {
   typescript: true,
-  apiVersion: "2023-10-16"
-})
+  apiVersion: "2023-10-16",
+});
 
 export async function GET(req: NextRequest, res: NextResponse) {
   const productId = req.nextUrl.searchParams.get("id") as string;
-  const product = await stripe.products.retrieve(
-    productId
-  );
-  const price = await stripe.prices.retrieve(
-    product.default_price as string
-  )
 
-  return NextResponse.json({
-    productName: product.name,
-    productPrice: price.unit_amount,
-    productPriceId: price.id,
-    productImage: product.images,
-    productDescription: product.description
-  })
+  try {
+    const product = await stripe.products.retrieve(productId);
+    const price = await stripe.prices.retrieve(product.default_price as string);
+
+    return NextResponse.json({
+      productName: product.name,
+      productPrice: price.unit_amount,
+      productPriceId: price.id,
+      productImage: product.images,
+      productDescription: product.description,
+      hatchedDate: product.metadata.HatchedDate, // Retrieve Hatched Date from metadata
+      weight: product.metadata.Weight, // Retrieve Weight from metadata
+    });
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return NextResponse.json({ error: "Failed to fetch product" }, { status: 500 });
+  }
 }
