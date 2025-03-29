eb4af91ebbdd5b986eb59b6aaad83c5e5e704ce8
Santiago Aguirre <49324282+santidevhmo@users.noreply.github.com>
2025-03-28 22:32:12 -0700

    Update NavigationLinks.tsx

--- app/components/NavBar/utils/NavigationLinks.tsx
+++ app/components/NavBar/utils/NavigationLinks.tsx
@@ -7,7 +7,6 @@ const NavigationLinks = () => {
       <ul className="flex mr-8 whitespace-nowrap gap-10 items-center">
         <li className="hover:text-blue-500"><Link href={"/blog"}>Blog</Link></li>
         <li className="hover:text-blue-500"><Link href={"/shop"}>Shop</Link></li>
-        <li className="hover:text-blue-500"><Link href={"/orders"}>My Orders</Link></li>
       </ul>
     </div>
   );
